d43bacd28db33f9e1c07fba69b5d25efa4d63467
abdulR1806 <abdulR1806@users.noreply.github.com>
2026-07-05 18:14:57 +0000

    deploy: e01a957ef86d37d6e3df051df922bc53ca3f60e8

--- data/site-data.js
+++ data/site-data.js
@@ -5,7 +5,7 @@ window.WORLD_CUP_LEADERBOARD_DATA = {
     "sourceWorkbook": "Google Sheet SKOR panitia",
     "officialScoreSheetUrl": "https://docs.google.com/spreadsheets/d/1Y8KS4EsNpYr8MTv0bUBEsYz-wNN6FaI9ke9J485N05o/edit?usp=sharing",
     "officialScoreSheetEmbedUrl": "https://docs.google.com/spreadsheets/d/e/2PACX-1vS1qP5REOzz6gjjbJf02gWWI3MzNnMCAfGfRPdLQQmSxibAMI2J3NJQCSK1SKZEfiUiS01mKxmmWFbA/pubhtml?gid=0&single=true&widget=true&headers=false",
-    "generatedAt": "2026-07-06T00:21:17+07:00",
+    "generatedAt": "2026-07-06T01:14:56+07:00",
     "timezone": "Asia/Jakarta",
     "resultDelayMinutes": 100,
     "scoring": "Klasemen memakai total resmi dari sheet SKOR panitia. Perhitungan W/L/D lama tidak lagi dipakai untuk menentukan peringkat."
